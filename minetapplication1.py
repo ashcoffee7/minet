153cb3d0cfb55a530f7578c9114c0f05d3f9f816
ashroice <66086211+ashroice@users.noreply.github.com>
2021-07-12 16:29:18 -0700

    Update minetapplication1.py

--- minetapplication1.py
+++ minetapplication1.py
@@ -14,7 +14,7 @@ from pycaret.regression import load_model, predict_model
 import streamlit as st
 
 from pycaret.classification import load_model
-model = load_model('knn7')
+model = load_model('knn7.pkl')
 
 def predict(model, input_df):
   predictions_df = predict_model(estimator=model, data=input_df)
