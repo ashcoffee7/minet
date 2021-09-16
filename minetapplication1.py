bd826f7b7a482c858a1fd8a23585a7fb62689b56
ashroice <66086211+ashroice@users.noreply.github.com>
2021-09-15 22:54:58 -0700

    Update minetapplication1.py

--- minetapplication1.py
+++ minetapplication1.py
@@ -27,6 +27,7 @@ def run():
   st.image(image,use_column_width = False)
   st.sidebar.info('MINet: A Novel Telemedicine Tool for Automatically Assessing Motivational Interviewing (MI) Conversations Using Natural Language Processing')
 
+import contractions
 file_upload = st.file_uploader('Upload Transcript for Rating +  Feedback. Ensure they are in PDF/TXT form and the counselor and patient portions are indicated with "c:" and "p:" at the beginning of every line.', type=["txt","pdf"])
 if file_upload is not None:
   file_contents2 = file_upload.read()
